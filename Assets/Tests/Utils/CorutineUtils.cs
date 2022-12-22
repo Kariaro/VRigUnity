2f4d9370fb279ddcd9c829b2cabe870df68694a3
Kariaro <25615845+Kariaro@users.noreply.github.com>
2022-12-22 22:43:46 +0100

    Base for test cases

---
+++ Assets/Tests/Utils/CorutineUtils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Assets.Tests.Utils {
+	public class CorutineUtils {
+		public static IEnumerator HandleExceptions(IEnumerator enumerator, Action<Exception> errors) {
+			var stack = new Stack<IEnumerator>();
+			stack.Push(enumerator);
+
+			while (stack.Count > 0) {
+				var currentEnumerator = stack.Peek();
+				object current;
+
+				try {
+					if (!currentEnumerator.MoveNext()) {
+						stack.Pop();
+						continue;
+					}
+
+					current = currentEnumerator.Current;
+				} catch (Exception e) {
+					errors(e);
+					yield break;
+				}
+
+				if (current is IEnumerator yieldedEnumerator) {
+					stack.Push(yieldedEnumerator);
+				} else {
+					yield return current;
+				}
+			}
+
+			errors(null);
+		}
+	}
+}
